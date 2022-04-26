95b69cd062180bfdd03418bf133c716212341711
mehdi <13610522>
2022-04-26 12:10:04 +0430

    update scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -2,15 +2,22 @@ package scheduler
 
 import (
 	"github.com/jasonlvhit/gocron"
+	"github.com/joho/godotenv"
+	"os"
+	"strconv"
 )
 
 func init() {
-	//readCustomerFundData()
-	//readCustomerFundInfo()
+	envErr := godotenv.Load()
+	if envErr != nil {
+		panic(envErr)
+	}
+	fund_customer_data, _ := strconv.ParseUint(os.Getenv("FUND_CUSTOMER_DATA_RUN_EVERY_SECOND"), 10, 64)
+	fund_customer_info, _ := strconv.ParseUint(os.Getenv("FUND_CUSTOMER_INFO_RUN_EVERY_SECOND"), 10, 64)
+
 	go func() {
-		//gocron.Every(60).Second().Do(readCustomerFundData)
-		gocron.Every(1200).Second().Do(readCustomerFundData)
-		gocron.Every(1200).Second().Do(readCustomerFundInfo)
+		gocron.Every(fund_customer_data).Second().Do(readCustomerFundData)
+		gocron.Every(fund_customer_info).Second().Do(readCustomerFundInfo)
 		<-gocron.Start()
 	}()
 }
