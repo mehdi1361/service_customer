06aa49933355214fd47bd4711a52be2e2e7fd9ac
mehdi <13610522>
2022-06-11 16:36:00 +0430

    update scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -15,12 +15,13 @@ func init() {
 	fund_customer_data, _ := strconv.ParseUint(os.Getenv("FUND_CUSTOMER_DATA_RUN_EVERY_SECOND"), 10, 64)
 	fund_customer_info, _ := strconv.ParseUint(os.Getenv("FUND_CUSTOMER_INFO_RUN_EVERY_SECOND"), 10, 64)
 	broker_customer_data, _ := strconv.ParseUint(os.Getenv("BROKER_CUSTOMER_DATA_RUN_EVERY_SECOND"), 10, 64)
+	broker_customer_info, _ := strconv.ParseUint(os.Getenv("BROKER_CUSTOMER_INFO_RUN_EVERY_SECOND"), 10, 64)
 
 	go func() {
 		gocron.Every(fund_customer_data).Second().Do(readCustomerFundData)
 		gocron.Every(fund_customer_info).Second().Do(readCustomerFundInfo)
 		gocron.Every(broker_customer_data).Second().Do(readBrokerCustomerData)
+		gocron.Every(broker_customer_info).Second().Do(readCustomerBrokerInfo)
 		<-gocron.Start()
 	}()
-	readCustomerBrokerInfo()
 }
